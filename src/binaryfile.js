4ed36143188d7dfec8cb7d93fcffc663fe5492c8
Michael Randolph <mrand01@gmail.com>
2014-04-21 12:12:23 -0400

    Removed document.write call and replaced with an appendChild call.
    Replaced document.write call that adds VBScript with a document.appendChild call due to the document.write call failing when script is loaded asynchronously

--- src/binaryfile.js
+++ src/binaryfile.js
@@ -141,13 +141,12 @@ function BinaryFile(strData, iDataOffset, iDataLength) {
     };
 }
 
-document.write(
-	"<script type='text/vbscript'>\r\n"
-	+ "Function IEBinary_getByteAt(strBinary, iOffset)\r\n"
-	+ "	IEBinary_getByteAt = AscB(MidB(strBinary,iOffset+1,1))\r\n"
-	+ "End Function\r\n"
-	+ "Function IEBinary_getLength(strBinary)\r\n"
-	+ "	IEBinary_getLength = LenB(strBinary)\r\n"
-	+ "End Function\r\n"
-	+ "</script>\r\n"
-);
+var js = document.createElement('script');
+js.type = 'text/vbscript';
+js.textContent = "Function IEBinary_getByteAt(strBinary, iOffset)\r\n" +
+    "	IEBinary_getByteAt = AscB(MidB(strBinary,iOffset+1,1))\r\n" +
+    "End Function\r\n" +
+    "Function IEBinary_getLength(strBinary)\r\n" +
+    "	IEBinary_getLength = LenB(strBinary)\r\n" +
+    "End Function\r\n";
+document.getElementsByTagName('head')[0].appendChild(js);
